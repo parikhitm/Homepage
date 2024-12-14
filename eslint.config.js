94fdc94bac0b7dd5423019354c0646292d0dc8ad
parikhitm <112902966+parikhitm@users.noreply.github.com>
2024-12-14 12:14:20 +0530

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// Convert .eslintrc.js to .eslint.config.js
+
+module.exports = {
+  languageOptions: {
+    globals: {
+      // Specify any globals here, separated by spaces
+      "global-variable1": true,
+      "global-variable2": true,
+    },
+  },
+};
